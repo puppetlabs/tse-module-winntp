b6a31b18bb71896d690a08789c18b027956a6283
ipcrm <mcadorette@gmail.com>
2016-10-10 17:51:05 -0400

    Set Reg keys/values to 32bit
    Prior to this commit this code assumed you are running on a 32bit
    agent.  If you use a 64bit agent it will fail to apply these changes
    with errors about not being able to munge data.  This commit explicitly
    sets these values to 32bit.

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,27 +9,27 @@ class winntp (
   # form the $ntp_servers String from the $servers Array.
   $ntp_servers = join(suffix($servers, ',0x9'), ' ')
 
-  registry_value { 'HKLM\\SYSTEM\\CurrentControlSet\\Services\\W32Time\\Parameters\\Type':
+  registry_value { '32:HKLM\\SYSTEM\\CurrentControlSet\\Services\\W32Time\\Parameters\\Type':
     type   => 'string',
     data   => 'NTP',
     notify => Service['w32time'],
   }
 
   # the list of servers in required space-delimited string format
-  registry_value { 'HKLM\\SYSTEM\\CurrentControlSet\\Services\\W32Time\\Parameters\\NtpServer':
+  registry_value { '32:HKLM\\SYSTEM\\CurrentControlSet\\Services\\W32Time\\Parameters\\NtpServer':
     type   => 'string',
     data   => $ntp_servers,
     notify => Service['w32time'],
   }
 
-  registry_value { 'HKLM\\SYSTEM\\CurrentControlSet\\Services\\W32Time\\TimeProviders\\NtpClient\\SpecialPollInterval':
+  registry_value { '32:HKLM\\SYSTEM\\CurrentControlSet\\Services\\W32Time\\TimeProviders\\NtpClient\\SpecialPollInterval':
     ensure => present,
     type   => 'dword',
     data   => $special_poll_interval,
     notify => Service['w32time'],
   }
 
-  registry_key { 'HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\DateTime\\Servers':
+  registry_key { '32:HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\DateTime\\Servers':
     ensure       => present,
     purge_values => $purge_unmanaged_servers,
   }
@@ -37,7 +37,7 @@ class winntp (
   # create a new numbered registry value for each ntp server (1 to $servers.length) 
   $servers.each |$index, $srv| {
     $i = $index + 1
-    registry_value { "HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\DateTime\\Servers\\${i}":
+    registry_value { "32:HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\DateTime\\Servers\\${i}":
       ensure => present,
       type   => 'string',
       data   => $srv,
@@ -46,21 +46,21 @@ class winntp (
   }
 
   # default setting is first ntp server (server 1)
-  registry_value { 'HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\DateTime\\Servers':
+  registry_value { '32:HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\DateTime\\Servers':
     ensure => present,
     type   => 'string',
     data   => '1',
     notify => Service['w32time'],
   }
 
-  registry_value { 'HKLM\\SYSTEM\\CurrentControlSet\\Services\\W32Time\\Config\\MaxPosPhaseCorrection':
+  registry_value { '32:HKLM\\SYSTEM\\CurrentControlSet\\Services\\W32Time\\Config\\MaxPosPhaseCorrection':
     ensure => present,
     type   => 'dword',
     data   => $max_pos_phase_correction,
     notify => Service['w32time'],
   }
 
-  registry_value { 'HKLM\\SYSTEM\\CurrentControlSet\\Services\\W32Time\\Config\\MaxNegPhaseCorrection':
+  registry_value { '32:HKLM\\SYSTEM\\CurrentControlSet\\Services\\W32Time\\Config\\MaxNegPhaseCorrection':
     ensure => present,
     type   => 'dword',
     data   => $max_neg_phase_correction,
